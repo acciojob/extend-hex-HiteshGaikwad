a4d1461a9c1b37e588360ae999eb263a78b8e223
HiteshGaikwad <116432379+HiteshGaikwad@users.noreply.github.com>
2023-06-10 12:44:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let n= shortHex.length;
+	let str='';
+	if(shortHex.charAT(0)=='#'){
+		str+=shortHex.charAT(0);
+	}
+	for(let i=0; i<3; i++){
+		let c=shortHex.charAt(i)
+		str+=c;
+		str+=c;
+	}
+	return str;
 };
 
 // Do not change the code below.
